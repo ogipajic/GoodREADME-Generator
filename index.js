062f03f580e595250f3a15d9c90ec4ebb0c040d5
Ognjen Pajic <ogipajic295@gmail.com>
2020-08-09 11:22:55 -0400

    question added

--- index.js
+++ index.js
@@ -7,7 +7,65 @@ result();
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        message: "Select license",
+        name: "License",
+        choices: [
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "How do you test your project?",
+        name: "Test"
+    },
+    
+    {
+        type: "input",
+        message: "Contact info for further questions",
+        name: "Questions"
+    },
+    {
+        type: "input",
+        message: "Your Github username",
+        name: "Username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email"
+    }
+    
 ];
 
 // function to write README file
